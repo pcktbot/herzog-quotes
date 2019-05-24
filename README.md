50b048e6dae69ff504309a60473904613b983f22
David Nave <pcktbot@gmail.com>
2019-05-24 06:53:03 -0700

    Working

---
+++ README.md
@@ -0,0 +1,4 @@
+> forked from [jamesseanwrite/ron-swanson-quotes](https://github.com/jamesseanwright/ron-swanson-quotes)
+
+# An API that returns random Werner Herzog quotes
+
---
+++ quotes.js
@@ -0,0 +1,7 @@
+module.exports = [
+  'Look into the eyes of a chicken and you will see real stupidity. It is a kind of bottomless stupidity, a fiendish stupidity. They are the most horrifying, cannibalistic and nightmarish creatures in the world.',
+  'In the face of the obscene, explicit malice of the jungle, which lacks only dinosaurs as punctuation, I feel like a half-finished, poorly expressed sentence in a cheap novel.',
+  'Facts do not convey truth. That\'s a mistake. Facts create norms, but truth creates illumination.',
+  'I believe the common denominator of the universe is not harmony; but chaos, hostility and murder.',
+  'If you’re purely after facts, please buy yourself the phone directory of Manhattan. It has four million times correct facts. But it doesn’t illuminate.'
+]
